9bea7602a9b241f4373c52a705289e45dcd40d3b
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-11-23 22:18:01 +0900

    厳密等価演算子へ変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 for (let number = 1; number <= 20; number++) {
-  if (number % 5 == 0 && number % 3 == 0) {
+  if (number % 5 === 0 && number % 3 === 0) {
     console.log("FizzBuzz");
-  } else if (number % 5 == 0) {
+  } else if (number % 5 === 0) {
     console.log("Buzz");
-  } else if (number % 3 == 0) {
+  } else if (number % 3 === 0) {
     console.log("Fizz");
   } else {
     console.log(number);
