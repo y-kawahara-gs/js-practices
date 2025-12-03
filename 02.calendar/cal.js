344375bc5d3fc20db0838baeadef6e4b2f50fa71
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-12-04 08:48:01 +0900

    三項演算子を論理和に変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -5,8 +5,8 @@ import minimist from "minimist";
 const argv = minimist(process.argv.slice(2));
 
 const today = new Date();
-const year = argv.y === undefined ? today.getFullYear() : argv.y;
-const month = argv.m === undefined ? today.getMonth() + 1 : argv.m;
+const year = argv.y || today.getFullYear();
+const month = argv.m || today.getMonth() + 1;
 
 const firstDate = new Date(year, month - 1, 1);
 const lastDate = new Date(year, month, 0);
