86514aae06729a0c4f5a748b28dcc76275c4c91a
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-12-03 10:44:35 +0900

    メモアプリ全体のシステムを管理するSystemControllerクラスの実装

---
+++ 04.class/systemController.js
@@ -0,0 +1,43 @@
+import { OptionParser } from "./optionparser.js";
+import readline from "node:readline";
+import { Memo } from "./memoClass.js";
+
+export class SystemController {
+  constructor() {
+    this.option = new OptionParser();
+  }
+  async run() {
+    const action = await this.option.decide_process();
+    switch (action) {
+      case "list":
+        await Memo.load_list();
+        break;
+      case "reference":
+        await Memo.load_reference();
+        break;
+      case "delete":
+        await Memo.delete();
+        break;
+      case "non-option": {
+        process.stdin.resume();
+        process.stdin.setEncoding("utf-8");
+        var lines = [];
+
+        const reader = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout,
+        });
+
+        reader.on("line", (line) => {
+          lines.push(line);
+        });
+
+        reader.on("close", () => {
+          const memo = new Memo(lines[0], lines.slice(1).join("\n"));
+          memo.save();
+        });
+        break;
+      }
+    }
+  }
+}
