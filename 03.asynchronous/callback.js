75fcda6bb8843550b0d8a0a0e6d4f76db825bbcf
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-12-10 16:01:59 +0900

    callbackのSQL文のWHERE句修正・パラメータの使用

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -6,16 +6,21 @@ let db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    db.run("INSERT INTO books (title) VALUES ('Railsの教科書')", function () {
-      console.log(this.lastID);
-      db.get(
-        "SELECT * FROM books WHERE title = 'Railsの教科書'",
-        (_error, book) => {
-          console.log(book);
-        },
-      );
-      db.close();
-    });
+    db.run(
+      "INSERT INTO books (title) VALUES (?)",
+      "Railsの教科書",
+      function () {
+        console.log(this.lastID);
+        db.get(
+          "SELECT * FROM books WHERE title = ?",
+          "Railsの教科書",
+          (_error, book) => {
+            console.log(book);
+          },
+        );
+        db.close();
+      },
+    );
   },
 );
 
@@ -26,13 +31,14 @@ db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   function () {
-    db.run("INSERT INTO books (title) VALUES (NULL)", (error) => {
+    db.run("INSERT INTO books (title) VALUES (?)", null, (error) => {
       if (error.code === "SQLITE_CONSTRAINT") {
         console.error(error.message);
       }
-      db.run("INSERT INTO books (title) VALUES ('Railsの教科書')", () => {
+      db.run("INSERT INTO books (title) VALUES (?)", "Railsの教科書", () => {
         db.get(
-          "SELECT * FROM book WHERE title = 'report'",
+          "SELECT * FROM book WHERE title = ?",
+          "Railsの教科書",
           (error, content) => {
             if (error.code === "SQLITE_ERROR") {
               console.error(error.message);
