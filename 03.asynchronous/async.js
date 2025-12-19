68e58e91d0aafbc94a6b831d43a2fd608e89d533
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-12-19 11:25:41 +0900

    抜けていたasync文を追加

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -3,69 +3,73 @@ import { dbRunPromise, dbGetPromise, dbClosePromise } from "./dbFunction.js";
 
 let db = new sqlite3.Database(":memory:");
 
-await dbRunPromise(
-  db,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
-let statementResult = await dbRunPromise(
-  db,
-  "INSERT INTO books (title) VALUES (?)",
-  "Railsの教科書",
-);
-console.log(statementResult.lastID);
-let book = await dbGetPromise(
-  db,
-  "SELECT * FROM books WHERE title = ?",
-  "Railsの教科書",
-);
-console.log(book);
-try {
-  await dbClosePromise(db);
-} catch (closeError) {
-  console.error(closeError.message);
-}
-
-db = new sqlite3.Database(":memory:");
-
-try {
+async () => {
   await dbRunPromise(
     db,
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   );
-  try {
-    statementResult = await dbRunPromise(
-      db,
-      "INSERT INTO books (title) VALUES (?)",
-      null,
-    );
-    console.log(statementResult.lastID);
-  } catch (error) {
-    if (error?.code === "SQLITE_CONSTRAINT") {
-      console.error(error.message);
-    } else {
-      throw error;
-    }
-  }
-  try {
-    book = await dbGetPromise(
-      db,
-      "SELECT * FROM book WHERE title = ?",
-      "Railsの教科書",
-    );
-    console.log(book);
-  } catch (error) {
-    if (error?.code === "SQLITE_ERROR") {
-      console.error(error.message);
-    } else {
-      throw error;
-    }
-  }
-} catch (error) {
-  console.error(error.message);
-} finally {
+  let statementResult = await dbRunPromise(
+    db,
+    "INSERT INTO books (title) VALUES (?)",
+    "Railsの教科書",
+  );
+  console.log(statementResult.lastID);
+  let book = await dbGetPromise(
+    db,
+    "SELECT * FROM books WHERE title = ?",
+    "Railsの教科書",
+  );
+  console.log(book);
   try {
     await dbClosePromise(db);
   } catch (closeError) {
     console.error(closeError.message);
   }
-}
+};
+
+db = new sqlite3.Database(":memory:");
+
+async () => {
+  try {
+    await dbRunPromise(
+      db,
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    );
+    try {
+      let statementResult = await dbRunPromise(
+        db,
+        "INSERT INTO books (title) VALUES (?)",
+        null,
+      );
+      console.log(statementResult.lastID);
+    } catch (error) {
+      if (error?.code === "SQLITE_CONSTRAINT") {
+        console.error(error.message);
+      } else {
+        throw error;
+      }
+    }
+    try {
+      let book = await dbGetPromise(
+        db,
+        "SELECT * FROM book WHERE title = ?",
+        "Railsの教科書",
+      );
+      console.log(book);
+    } catch (error) {
+      if (error?.code === "SQLITE_ERROR") {
+        console.error(error.message);
+      } else {
+        throw error;
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    try {
+      await dbClosePromise(db);
+    } catch (closeError) {
+      console.error(closeError.message);
+    }
+  }
+};
